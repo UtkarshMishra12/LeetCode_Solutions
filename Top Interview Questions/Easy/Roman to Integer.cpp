6b1fecac6de5e7627e167934a40f9d2959b864b9
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-05 10:20:27 +0530

    DAY-4

---
+++ Top Interview Questions/Easy/Roman to Integer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int romanToInt(string s) {
+    vector<int>v(256);
+    v['I']=1,v['V']=5,v['X']=10,v['L']=50,v['C']=100,v['D']=500,v['M']=1000;
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            if(v[s[i]]<v[s[i+1]]){
+                sum=sum-v[s[i]];
+            }
+            else{
+                sum=sum+v[s[i]];
+            }
+        }
+    return sum;  
+}
+int main() {
+    string s;cin>>s;
+    cout<<romanToInt(s);
+	// your code goes here
+	return 0;
+}
