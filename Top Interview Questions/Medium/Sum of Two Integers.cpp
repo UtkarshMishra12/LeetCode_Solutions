8435fdefd1b2567570bc9d9a84534acdf67e2319
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-06 22:51:40 +0530

    Medium

---
+++ Top Interview Questions/Medium/Sum of Two Integers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int getSum(int a, int b) {
+    int z=a&b;
+    int zz=a^b;
+    return (zz+2*z);
+}
+int main() {
+    //Call Fun
+	// your code goes here
+	return 0;
+}
