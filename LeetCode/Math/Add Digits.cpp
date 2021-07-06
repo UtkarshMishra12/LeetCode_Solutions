4f7ced460e79bd04931e4e38851a448b1047e634
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-06 21:35:03 +0530

    Math

---
+++ LeetCode/Math/Add Digits.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int addDigits(int num) {
+    if (num == 0) return 0;
+    if (num % 9 == 0) return 9;
+    return num % 9;
+}
+
+int main() {
+    //Call Fun
+	// your code goes here
+	return 0;
+}
