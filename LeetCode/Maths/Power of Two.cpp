caedcc45f353c45a4de38525f45b471404af0393
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-06 21:15:51 +0530

    Math

---
+++ LeetCode/Maths/Power of Two.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+bool isPowerOfTwo(int n) {
+   return bitset<33>(n).count() == 1;
+}
+}
+int main() {
+    //Call Fun
+	// your code goes here
+	return 0;
+}
