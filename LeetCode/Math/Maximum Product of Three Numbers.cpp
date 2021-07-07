341f066e869e4c03e4c303d558cc52d727443602
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-07 14:22:11 +0530

    Math

---
+++ LeetCode/Math/Maximum Product of Three Numbers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int maximumProduct(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n=nums.size();
+    return max(nums[n-1]*nums[n-2]*nums[n-3], nums[0]*nums[1]*nums[n-1]);
+}
+int main() {
+    //Call Fun
+	// your code goes here
+	return 0;
+}
