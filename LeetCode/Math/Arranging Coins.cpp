9193e8a2ffd9a465b2d3706cc07892652161c4bf
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-07 16:31:59 +0530

    Math

---
+++ LeetCode/Math/Arranging Coins.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int arrangeCoins(int n) {
+    return (int)(sqrt(2 * (long)n + 0.25) - 0.5);
+}
+int main() {
+    //Call Fun
+	// your code goes here
+	return 0;
+}
