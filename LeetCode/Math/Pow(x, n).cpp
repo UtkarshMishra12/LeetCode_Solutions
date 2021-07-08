af76295315a10706e5db740ad5fb6798dba64211
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-08 11:37:28 +0530

    Math

---
+++ LeetCode/Math/Pow(x, n).cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+double myPow(double x, int n) {
+    return pow(x,n);
+}
+int main() {
+    //Call Fun
+	// your code goes here
+	return 0;
+}
