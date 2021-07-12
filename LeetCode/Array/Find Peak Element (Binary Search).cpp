970f31f57951123d63bcb84b038741a0e516a221
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-12 15:12:01 +0530

    Array

---
+++ LeetCode/Array/Find Peak Element (Binary Search).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int findPeakElement(vector<int>& nums) {
+    int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+    return l;
+}
+int main() {
+    //Call Func
+	// your code goes here
+	return 0;
+}
