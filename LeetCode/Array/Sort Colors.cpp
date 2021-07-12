5401b6e65841c93567beeeefe6e9bec0fea73d33
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-12 10:06:35 +0530

    Array

---
+++ LeetCode/Array/Sort Colors.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+    }
+int main() {
+    //Call Func
+	// your code goes here
+	return 0;
+}
