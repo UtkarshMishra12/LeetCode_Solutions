a3befb9c39885c964c3bf61e05b13af24dbb3cf4
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-07-14 18:05:41 +0530

    String

---
+++ LeetCode/String/Student Attendance Record I.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+bool checkRecord(string s) {
+       if(count(s.begin(),s.end(),'A')<2 && s.find("LLL")==string::npos)
+		return true;
+	
+    return false;
+}
+int main() {
+    //Call Func
+	// your code goes here
+	return 0;
+}
