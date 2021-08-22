da67dc8a1caa612a5b8a963fc2e6c7bffc7a0c8e
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-08-22 13:33:46 +0530

    Math

---
+++ LeetCode/Math/Minimum Operations to Make Array Equal.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+
+int minOperations(int n) {
+        return n*(n/2)-(n/2)*(n/2);
+    }
+int main() {
+   
+	// your code goes here
+	return 0;
+}
+ 
