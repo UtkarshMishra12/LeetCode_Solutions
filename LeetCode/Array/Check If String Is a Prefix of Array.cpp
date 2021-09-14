0d6ee20367bd0c92e5c16330e5c52c259dfd6688
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-09-14 23:16:45 +0530

    Array

---
+++ LeetCode/Array/Check If String Is a Prefix of Array.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+
+bool isPrefixString(string s, vector<string>& words) {
+        string str = "";
+        int i = 0;
+        while (i < words.size() and str != s){
+            str += words[i];
+            i++;
+        }
+        
+        return str == s;
+    }
+
+int main() {
+    //Call Func
+	// your code goes here
+	return 0;
+}
+ 
