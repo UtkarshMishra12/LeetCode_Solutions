127d484efd3acb565123ffc8dd534d70a81a67c6
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-09-25 15:03:01 +0530

    DP

---
+++ LeetCode/Dynamic Programming/Divisor Game.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+
+  bool divisorGame(int n) {
+        return !(n%2);
+    }
+
+int main() {
+    //Call Func
+	// your code goes here
+	return 0;
+}
+ 
