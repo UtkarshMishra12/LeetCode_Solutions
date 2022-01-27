a0f40172f815064791117f999acc6fb2327b4c49
Utkarsh Mishra <nasautkarsh@gmail.com>
2022-01-27 19:05:36 +0530

    Linked List

---
+++ LeetCode/Linked List/Copy List with Random Pointer.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+#define pb push_back
+
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+ Node* copyRandomList(Node* head) {
+          Node *iter = head; 
+          Node *front = head;
+
+          // First round: make copy of each node,
+          // and link them together side-by-side in a single list.
+          while (iter != NULL) {
+            front = iter->next;
+
+            Node *copy = new Node(iter->val);
+            iter->next = copy;
+            copy->next = front;
+
+            iter = front;
+          }
+
+          // Second round: assign random pointers for the copy nodes.
+          iter = head;
+          while (iter != NULL) {
+            if (iter->random != NULL) {
+              iter->next->random = iter->random->next;
+            }
+            iter = iter->next->next;
+          }
+
+          // Third round: restore the original list, and extract the copy list.
+          iter = head;
+          Node *pseudoHead = new Node(0);
+          Node *copy = pseudoHead;
+
+          while (iter != NULL) {
+            front = iter->next->next;
+
+            // extract the copy
+            copy->next = iter->next;
+
+            // restore the original list
+            iter->next = front;
+              
+            copy = copy -> next; 
+            iter = front;
+          }
+
+          return pseudoHead->next;
+    }
+
+int main() {
+
+    
+    //Call Func
+	// your code goes here
+	return 0;
+}
+ 
