93f03771ec0a3c41b4e5d59ef104ea955f326a28
Utkarsh Mishra <nasautkarsh@gmail.com>
2022-04-01 18:44:33 +0530

    Binary Tree

---
+++ LeetCode/Binary Tree/Vertical Order Traversal of a Binary Tree.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+#define pb push_back
+
+
+vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, map<int, multiset<int>>> nodes;
+        queue<pair<TreeNode*, pair<int, int>>> todo;
+        todo.push({root, {0, 0}});
+        while (!todo.empty()) {
+            auto p = todo.front();
+            todo.pop();
+            TreeNode* node = p.first;
+            int x = p.second.first, y = p.second.second;
+            nodes[x][y].insert(node -> val);
+            if (node -> left) {
+                todo.push({node -> left, {x - 1, y + 1}});
+            }
+            if (node -> right) {
+                todo.push({node -> right, {x + 1, y + 1}});
+            }
+        }
+        vector<vector<int>> ans;
+        for (auto p : nodes) {
+            vector<int> col;
+            for (auto q : p.second) {
+                col.insert(col.end(), q.second.begin(), q.second.end());
+            }
+            ans.push_back(col);
+        }
+        return ans;
+    }
+
+int main() {
+
+    
+    //Call Func
+	// your code goes here
+	return 0;
+}
+ 
