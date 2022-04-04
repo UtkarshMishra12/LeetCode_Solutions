866625f04c945e91acf7bb3ccbcd9e30d8f9fd2d
Utkarsh Mishra <nasautkarsh@gmail.com>
2022-04-04 23:37:14 +0530

    Linked List

---
+++ LeetCode/Linked List/Swapping Nodes in a Linked List.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+#define pb push_back
+
+ ListNode* swapNodes(ListNode* head, int k) {
+       ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
+        while (--k)
+            ptr1 = ptr1->next;
+        
+        kth = ptr1;
+        ptr1 = ptr1->next;
+        
+        while (ptr1) {
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        swap(ptr2->val, kth->val);
+        return head;
+    }
+
+int main() {
+
+    
+    //Call Func
+	// your code goes here
+	return 0;
+}
+ 
