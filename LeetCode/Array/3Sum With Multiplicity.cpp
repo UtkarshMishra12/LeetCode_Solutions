b6a327cff4c2a4599d622ef630668c8c842ba5ec
Utkarsh Mishra <nasautkarsh@gmail.com>
2022-04-06 21:31:30 +0530

    Array

---
+++ LeetCode/Array/3Sum With Multiplicity.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+#define pb push_back
+
+int threeSumMulti(vector<int>& A, int target) {
+        unordered_map<int, long> c;
+        for (int a : A) c[a]++;
+        long res = 0;
+        for (auto it : c)
+            for (auto it2 : c) {
+                int i = it.first, j = it2.first, k = target - i - j;
+                if (!c.count(k)) continue;
+                if (i == j && j == k)
+                    res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
+                else if (i == j && j != k)
+                    res += c[i] * (c[i] - 1) / 2 * c[k];
+                else if (i < j && j < k)
+                    res += c[i] * c[j] * c[k];
+            }
+        return res % int(1e9 + 7);
+    }
+
+int main() {
+
+    
+    //Call Func
+	// your code goes here
+	return 0;
+}
+ 
