ab3d3e7ecfc60caf43392bed0179ebc4b35e9469
Utkarsh Mishra <nasautkarsh@gmail.com>
2022-04-22 17:33:01 +0530

    Array

---
+++ LeetCode/Array/Design HashMap.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+#define pb push_back
+
+ int data[1000001];
+    MyHashMap() {
+        fill(data, data + 1000000, -1);
+    }
+    void put(int key, int val) {
+        data[key] = val;
+    }
+    int get(int key) {
+        return data[key];
+    }
+    void remove(int key) {
+        data[key] = -1;
+    }
+
+int main() {
+
+    
+    //Call Func
+	// your code goes here
+	return 0;
+}
+ 
