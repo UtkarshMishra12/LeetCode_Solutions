45403508cd417c8c2e47f9c91fe690e275a56555
Utkarsh Mishra <nasautkarsh@gmail.com>
2022-04-23 22:47:02 +0530

    String

---
+++ LeetCode/String/Decode it.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds;
+#define ll long long int
+#define pb push_back
+
+ unordered_map<string, string> codeDB, urlDB;
+    const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    
+    string getCode() {
+        string code = "";
+        for (int i = 0; i < 6; i++) code += chars[rand() % 62];
+        return "http://tinyurl.com/" + code;
+    }
+    
+    string encode(string longUrl) {
+        if (urlDB.find(longUrl) != urlDB.end()) return urlDB[longUrl];
+        string code = getCode();
+        while (codeDB.find(code) != codeDB.end()) code = getCode();
+        codeDB[code] = longUrl;
+        urlDB[longUrl] = code;
+        return code;
+    }
+
+    string decode(string shortUrl) {
+        return codeDB[shortUrl];
+    }
+};
+
+int main() {
+
+    
+    //Call Func
+	// your code goes here
+	return 0;
+}
+ 
